d626b5ffab1bc7b2c6e784e78e78557af28bcdb0
Lucas Sodre <lucasmeinen@yahoo.com.br>
2023-02-03 13:45:38 -0300

    Criando validações nos campos do objeto filme

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class FilmeController
+public class FilmeController: ControllerBase
 {
+    private static List<Filme> filmes = new List<Filme>();
 
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
 }
