16a4648bb23aa1dcb1dfff200570f01eab5678a2
Lucas Sodre <lucasmeinen@yahoo.com.br>
2023-02-03 12:27:53 -0300

    Criando models Filme

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+
+}
